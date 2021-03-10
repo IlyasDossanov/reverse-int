b24d17412d32294568731cad86d85e7775499f28
Ilyas <ilyasdossanov@gmail.com>
2021-03-10 22:40:33 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let m = ''
+    n+=''
+  for(let i = n.length; i > -1; i--){
+      if(n.charAt(i)!='-'){
+         m += n.charAt(i);
+      }
+  }
+  return(m);
 }
